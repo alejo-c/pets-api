d129a48d1574e1ba2702f529a9f20de97ef71922
Alejandro Castrillón <4lejo.castrillon@gmail.com>
2023-12-17 14:04:17 -0500

    Edit Pet Model
    Columns type and adoption_status added.
    
    Pet types were included.
    
    functions getPet and formatPet were added.

--- models/pet-model.js
+++ models/pet-model.js
@@ -1,21 +1,38 @@
 import { DataTypes } from "sequelize"
 import { db } from "../database/config.db.js"
+import { adoptionStatuses } from './adoption-request-model.js'
 
-const pet = db.define('pet', {
+export const Pet = db.define('pet', {
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         allowNull: false,
         autoIncrement: true
     },
+    type: {
+        type: DataTypes.CHAR,
+        allowNull: false
+    },
     name: {
         type: DataTypes.STRING,
         allowNull: false
     },
     age: {
-        type: DataTypes.INTEGER,
-        allowNull: true
+        type: DataTypes.INTEGER
+    },
+    adoption_status: {
+        type: DataTypes.CHAR,
+        allowNull: false,
+        defaultValue: '1'
     }
 })
 
-export { pet }
+export const petTypes = { 'C': 'Cat', 'D': 'Dog' }
+
+export const getPet = id => Pet.findByPk(id)
+
+export const formatPet = pet => ({
+    ...pet.dataValues,
+    type: petTypes[pet.type],
+    adoption_status: adoptionStatuses[pet.adoption_status]
+})
