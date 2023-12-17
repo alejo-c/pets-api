e2843482c4c9ed9c4af1e4ed583deea2c43a3f30
Alejandro Castrillón <4lejo.castrillon@gmail.com>
2023-12-17 16:42:46 -0500

    Import mysql2 as dialect module in sequelize db

--- database/config.db.js
+++ database/config.db.js
@@ -2,11 +2,12 @@ import { Sequelize } from "sequelize"
 import 'dotenv/config.js'
 
 const db = new Sequelize({
-    dialect: 'mysql',
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWD,
     database: process.env.DB_NAME,
+    dialect: 'mysql',
+    dialectModule: mysql2,
 })
 
 export { db }
