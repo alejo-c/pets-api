b4d75c3773e9b67d3670c73a445fe421062f2884
Alejandro Castrillón <4lejo.castrillon@gmail.com>
2023-12-17 16:27:14 -0500

    Add dialectModule in sequelize config

--- database/config.db.js
+++ database/config.db.js
@@ -1,8 +1,10 @@
 import { Sequelize } from "sequelize"
+import mysql2 from 'mysql2'
 import 'dotenv/config.js'
 
 const db = new Sequelize({
     dialect: 'mysql',
+    dialectModule: mysql2,
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWD,
