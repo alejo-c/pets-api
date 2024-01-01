4c1eaa9d2c49b79efc72d6fc526b49f8ea2bc238
Alejandro Castrillón <4lejo.castrillon@gmail.com>
2024-01-01 17:35:26 -0500

    Rename request route to adoption-request

--- src/app.js
+++ src/app.js
@@ -1,38 +1,37 @@
-import express from "express"
+import express from 'express'
 import cors from 'cors'
 
-import { petsRouter } from "../routes/pets-router.js"
-import { adopterRouter } from '../routes/adopter-router.js'
-import { requestRouter } from '../routes/adoption-request-router.js'
-import { db } from "../database/config.db.js"
+import { db } from '../database/config.db.js'
+import { petsRouter } from '../routes/pets-router.js'
+import { adoptersRouter } from '../routes/adopters-router.js'
+import { requestsRouter } from '../routes/adoption-requests-router.js'
 
 const app = express()
 const PORT = 8000
 
-db.authenticate().then(() => {
-    console.log('Database connected successfully')
-}).catch(err => {
-    console.error(`Error at database connection: ${err}`)
-})
-
 app.use(express.json())
 app.use(cors())
+
+app.use('/pets', petsRouter)
+app.use('/adopters', adoptersRouter)
+app.use('/adoption-requests', requestsRouter)
+
+app.get('/', (req, res) => res.status(301).redirect(
+    'https://github.com/alejo-c/pets-api#api-usage'
+))
 app.use((req, res, next) => {
     res.jsonPretty = jsonObj => res.send(JSON.stringify(jsonObj, null, 2))
     next()
 })
-app.use('/pets', petsRouter)
-app.use('/adopters', adopterRouter)
-app.use('/requests', requestRouter)
 
-app.get('/', (req, res) => res.status(301).redirect(
-    'https://github.com/alejo-c/pets-api?tab=readme-ov-file#api-usage'
-))
+db.authenticate()
+    .then(() => console.log('Database connected successfully'))
+    .catch(err => console.error(`Error at database connection: ${err}`))
 
-db.sync().then(() => {
-    app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`))
-}).catch(err => {
-    console.error(`Error at database sync: ${err}`);
-})
+db.sync().then(() =>
+    app.listen(PORT, () =>
+        console.log(`Server started at http://localhost:${PORT}`)
+    )
+).catch(err => console.error(`Error at database sync: ${err}`))
 
 export default app
