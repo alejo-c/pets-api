7065cbd20f2e9b0e0220f755decb2a6f1fca353c
Alejandro Castrillón <4lejo.castrillon@gmail.com>
2024-01-01 17:32:30 -0500

    Change router name

--- routes/adopters-router.js
+++ routes/adopters-router.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import * as controller from "../controllers/adopters-controller.js"
+import * as controller from '../controllers/adopters-controller.js'
 
 const router = express.Router()
 
 router.get('/', controller.readAdopters)
 router.get('/:id', controller.readAdopter)
-router.post('/create', controller.createAdopter)
+router.post('/register', controller.createAdopter)
 router.put('/update/:id', controller.updateAdopter)
 router.delete('/delete/:id', controller.deleteAdopter)
 
