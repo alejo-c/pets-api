d2f01c6ec40d7108f471e2de96c77c3ccfe3d7bc
Alejandro Castrillón <4lejo.castrillon@gmail.com>
2024-01-01 17:33:50 -0500

    Change router name and include new endpoints

--- routes/adoption-requests-router.js
+++ routes/adoption-requests-router.js
@@ -1,17 +1,19 @@
 import express from 'express'
-import * as controller from "../controllers/adoption-requests-controller.js"
+import * as controller from '../controllers/adoption-requests-controller.js'
 
 const router = express.Router()
 
 router.get('/', controller.readRequests)
 router.get('/:id', controller.readRequest)
-router.post('/create', controller.createRequest)
+router.get('/:id/pet', controller.readRequestPet)
+router.get('/:id/adopter', controller.readRequestAdopter)
+router.post('/register', controller.createRequest)
 router.put('/update/:id', controller.updateRequest)
 router.put('/approve/:id', controller.approveRequest)
 router.put('/reject/:id', controller.rejectRequest)
 router.delete('/delete/:id', controller.deleteRequest)
 
-export { router as requestRouter }
+export { router as requestsRouter }
 
 
 
