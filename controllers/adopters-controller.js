83b026332b9ae8f5c24042af72f7c0189a5a5b34
Alejandro Castrillón <4lejo.castrillon@gmail.com>
2024-01-01 21:30:40 -0500

    Add more description at response

--- controllers/adopters-controller.js
+++ controllers/adopters-controller.js
@@ -1,14 +1,21 @@
-import { Adopter, getAdopter } from '../models/adopter-model.js'
+import { Adopter, getAdopterById } from '../models/adopter-model.js'
 
 export const createAdopter = async (req, res) => {
-    if (req.body.name == undefined)
-        return res.status(400).jsonPretty({ type: 'error', message: 'The adopter name is required' })
+    if (req.body.name === undefined) return res.status(400).jsonPretty({
+        type: 'error',
+        message: 'The adopter name is required'
+    })
+    if (req.body.contact === undefined) return res.status(400).jsonPretty({
+        type: 'error',
+        message: 'The adopter contact is required'
+    })
 
     const adopter = await Adopter.create(req.body)
+
     res.status(201).jsonPretty({
         type: 'success',
-        message: `Adopter [${adopter.id}] created successfully`,
-        adopter
+        message: `Adopter ${adopter.name} (id: ${id}) created successfully`,
+        created: adopter
     })
 }
 
@@ -16,49 +23,65 @@ export const readAdopters = async (req, res) => {
     const adopters = await Adopter.findAll()
     const length = adopters.length
 
-    if (length == 0)
-        return res.status(500).jsonPretty({ type: 'error', message: 'There is no adopters', length })
-    res.status(200).jsonPretty({ adopters, length })
+    if (length === 0) return res.status(500).jsonPretty({
+        type: 'error',
+        message: 'There is no adopters',
+        length
+    })
+    res.status(200).jsonPretty({ collection: adopters, length })
 }
 
 export const readAdopter = async (req, res) => {
     const id = req.params.id
-    const adopter = await getAdopter(id)
+    const adopter = await getAdopterById(id)
 
-    if (adopter == null)
-        return res.status(404).jsonPretty({ type: 'error', message: `Adopter [${id}] not found` })
-    res.status(200).jsonPretty({ adopter })
+    if (adopter === null) return res.status(404).jsonPretty({
+        type: 'error',
+        message: `Adopter with id ${id} not found`
+    })
+    res.status(200).jsonPretty({ object: adopter })
 }
 
 export const updateAdopter = async (req, res) => {
     const id = req.params.id
+    const { name, address, contact } = req.body
 
-    if (!req.body.name && !req.body.address && !req.body.contact)
-        return res.status(400).jsonPretty({ error: 'There is not valid data for update' })
+    const adopter = await getAdopterById(id)
+    if (adopter === null) return res.status(404).jsonPretty({
+        type: 'error',
+        message: `Adopter with id ${id} does not exists`
+    })
 
-    const adopter = await getAdopter(id)
-    if (adopter == null)
-        return res.status(404).jsonPretty({ type: 'error', message: `Adopter [${id}] does not exists` })
+    const isInvalidData = name === undefined
+        && address === undefined
+        && contact === undefined
+    if (isInvalidData) return res.status(400).jsonPretty({
+        type: 'error',
+        message: 'There is not valid data for update'
+    })
 
     adopter.update(req.body)
+    
     res.status(200).jsonPretty({
         type: 'success',
-        message: `Adopter [${id}] updated successfully`,
-        adopter
+        message: `Adopter ${adopter.name} (id: ${id}) updated successfully`,
+        updated: adopter
     })
 }
 
 export const deleteAdopter = async (req, res) => {
     const id = req.params.id
-    const adopter = await getAdopter(id)
+    const adopter = await getAdopterById(id)
 
-    if (adopter == null)
-        return res.status(404).jsonPretty({ type: 'error', message: `Adopter [${id}] does not exists` })
+    if (adopter === null) return res.status(404).jsonPretty({
+        type: 'error',
+        message: `Adopter with id ${id} does not exists`
+    })
 
     adopter.destroy()
     res.status(200).jsonPretty({
         type: 'success',
-        message: `Adopter [${id}] deleted successfully`,
-        adopter
+        message: `Adopter ${adopter.name} (id: ${id}) deleted successfully`,
+        deleted: adopter
     })
 }
