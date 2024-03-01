b354294bdfcf6cb87ff24ea34bf9b120682ffabc
alejo-c <4lejo.castrillon@gmail.com>
2024-02-29 19:41:55 -0500

    Add port enviroment variable

--- database/config.db.js
+++ database/config.db.js
@@ -3,12 +3,13 @@ import mysql2 from 'mysql2'
 import 'dotenv/config.js'
 
 const db = new Sequelize({
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWD,
-    database: process.env.DB_NAME,
-    dialect: 'mysql',
-    dialectModule: mysql2,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  dialect: 'mysql',
+  dialectModule: mysql2,
 })
 
 export { db }
