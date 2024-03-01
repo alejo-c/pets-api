562cdad4cc29e8bc4dc2284eb3529ad2bf36cb17
alejo-c <4lejo.castrillon@gmail.com>
2024-03-01 16:19:50 -0500

    Capture enviroment app PORT or use 8000 as default

--- src/app.js
+++ src/app.js
@@ -1,37 +1,39 @@
 import express from 'express'
 import cors from 'cors'
 
+import 'dotenv/config.js'
+
 import { db } from '../database/config.db.js'
 import { petsRouter } from '../routes/pets-router.js'
 import { adoptersRouter } from '../routes/adopters-router.js'
 import { requestsRouter } from '../routes/adoption-requests-router.js'
 
 const app = express()
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 
 app.use(express.json())
 app.use(cors())
 
 app.use((req, res, next) => {
-    res.jsonPretty = jsonObj => res.send(JSON.stringify(jsonObj, null, 2))
-    next()
+  res.jsonPretty = jsonObj => res.send(JSON.stringify(jsonObj, null, 2))
+  next()
 })
 
 app.get('/', (req, res) => res.status(301).redirect(
-    'https://github.com/alejo-c/pets-api#api-usage'
+  'https://github.com/alejo-c/pets-api#api-usage'
 ))
 app.use('/pets', petsRouter)
 app.use('/adopters', adoptersRouter)
 app.use('/adoption-requests', requestsRouter)
 
 db.authenticate()
-    .then(() => console.log('Database connected successfully'))
-    .catch(err => console.error(`Error at database connection: ${err}`))
+  .then(() => console.log('Database connected successfully'))
+  .catch(err => console.error(`Error at database connection: ${err}`))
 
 db.sync().then(() =>
-    app.listen(PORT, () =>
-        console.log(`Server started at http://localhost:${PORT}`)
-    )
+  app.listen(PORT, () =>
+    console.log(`Server started at http://localhost:${PORT}`)
+  )
 ).catch(err => console.error(`Error at database sync: ${err}`))
 
 export default app
